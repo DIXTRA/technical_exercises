5f418574bd7d06155bd01f8fe1a7fdbaa26d6101
Mariano Amado <28148619+MarianoAmado@users.noreply.github.com>
2023-10-26 19:42:30 -0300

    feat: add exercise 3

---
+++ src/exercises/ex3.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+const veryExpensiveFunction = (n) => `<calculated value for ${n}>`;
+
+function Exercise3() {
+  const [counter, setCounter] = useState(0);
+  const [active, setActive] = useState(true);
+
+  const value = veryExpensiveFunction(counter);
+
+  const onIncrement = () => setCounter(counter + 1);
+  const toggleActive = () => setActive(!active);
+
+  return (
+    <div className="home-wrapper" style={{ width: 500 }}>
+      <h2 className={active ? 'active' : ''}>{value}</h2>
+      <button onClick={onIncrement}>Increment</button>
+      {' | '}
+      <button onClick={toggleActive}>Toggle Active</button>
+    </div>
+  );
+}
+
+export default Exercise3;
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 import Exercise1 from './exercises/ex1.jsx';
+import Exercise2 from './exercises/ex2.jsx';
+import Exercise3 from './exercises/ex3.jsx';
 
 const router = createBrowserRouter([
   {
@@ -13,6 +15,14 @@ const router = createBrowserRouter([
   {
     path: '/ex1',
     element: <Exercise1 />,
+  },
+  {
+    path: '/ex2',
+    element: <Exercise2 />,
+  },
+  {
+    path: '/ex3',
+    element: <Exercise3 />,
   },
 ]);
 
